17d866358b8344a4cc4cffcaf0473723fcaee9bc
lquirozm <langelq59@gmail.com>
2025-03-05 09:37:41 -0500

    Eliminar console.log del modo produccion

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,21 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
- 
-    images: {
-      remotePatterns: [
-        {
-          protocol: 'https',
-          hostname: 'utfs.io',
-          port: '',
-          pathname: '/f/**',
-          search: '',
-        },
-      ],
-  }
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production"
+  },
+  
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "utfs.io",
+        port: "",
+        pathname: "/f/**",
+        search: "",
+      },
+    ],
+  },
 };
 
 export default nextConfig;
