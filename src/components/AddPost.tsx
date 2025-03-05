eb60bcdf53b0b3104f1ed041156b3b9adcb95e31
lquirozm <langelq59@gmail.com>
2025-03-05 15:25:42 -0500

    Agregar animacion a la ventana de Agregar Post

--- src/components/AddPost.tsx
+++ src/components/AddPost.tsx
@@ -1,3 +1,4 @@
+import "animate.css"
 import { IoIosCloseCircle } from "react-icons/io";
 import ImageUpload from "./ImageUpload";
 import { useState } from "react";
@@ -55,7 +56,7 @@ const AddPost: React.FC<AddPostProps> = ({ setShowPopUp, setPosts }) => {
 
   return (
     <div className="fixed inset-0 bg-black bg-opacity-50 flex justify-center items-center z-50">
-      <div className="bg-white rounded-lg shadow-lg w-full max-w-md p-8 relative max-h-[100vh] dark:bg-[#121212]">
+      <div className="animate__animated animate__fadeInUp bg-white rounded-lg shadow-lg w-full max-w-md p-8 relative max-h-[100vh] dark:bg-[#121212]">
         <div className="flex justify-end">
           <IoIosCloseCircle
             size={30}
